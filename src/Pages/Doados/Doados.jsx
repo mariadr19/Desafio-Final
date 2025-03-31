0ba972b5e08b86baeb25f4b40ab67509d60f7ea9
mariadr19 <mariadrosario669@gmail.com>
2025-03-31 15:32:34 -0300

    "concluido"		modified:   package-lock.json "concluido"		modified:   package.json "concluido"		modified:   src/Pages/Doados/Doados.jsx "concluido"		modified:   src/Pages/Doados/doados.module.scss "concluido"		modified:   src/Pages/QueroDoar/QueroDoar.jsx "concluido"		new file:   src/Pages/QueroDoar/test.jsx

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,21 @@
 import protagonista from '../../assets/img/livro-o-protagonista.png'
 import D from '../Doados/doados.module.scss'
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 
 export default function Doados(){
+
+    const [livros, setLivros] = useState([])
+    const getLivros = async() => {
+        const response = await axios.get("https://projeto-api-livros.onrender.com/livros")
+        setLivros(response.data)
+    }
+
+    useEffect(() => {
+        getLivros()
+    }, [])
+
+
     return(
         <section className={D.boxDoados}>
             <h2>Livros Doados</h2>
@@ -12,6 +26,14 @@ export default function Doados(){
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
                 </article>
+                {livros.map((livro) => (
+                    <article key={livro.id}>
+                        <img src={livro.imagem_url} alt="" />
+                        <h3>{livro.titulo}</h3>
+                        <p>{livro.autor}</p>
+                        <p>{livro.categoria}</p>
+                    </article>
+                ))}
             </section>
         </section>
             
